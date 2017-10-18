5a474c68dea6c5e0b3273192d140cf4b791e692e
COTE <GrCOTE7@GMail.com>
2017-10-18 19:43:19 +0200

    >Page contact que pour les çinvités ( = non logués)

--- app/Http/Controllers/Front/ContactController.php
+++ app/Http/Controllers/Front/ContactController.php
@@ -16,7 +16,7 @@ class ContactController extends Controller
      */
     public function __construct()
     {
-        //$this->middleware('guest');
+        $this->middleware('guest');
     }
 
     /**
