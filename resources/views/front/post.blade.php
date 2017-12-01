6baeead1377a0ca967533e609b8e953c6cf851b0
imanghafoori <imanghafoori1@gmail.com>
2017-12-01 20:13:20 +0330

    extract partial

--- resources/views/front/post.blade.php
+++ resources/views/front/post.blade.php
@@ -110,28 +110,7 @@
                 @endif
 			@endif
 
-			<!-- respond -->
-			<div class="respond">
-
-			    <h3>@lang('Leave a Comment')</h3>
-                @if (Auth::check())
-    				<form method="post" action="{{ route('posts.comments.store', [$post->id]) }}">
-                        {{ csrf_field() }}
-    					<div class="message form-field">
-                            @if ($errors->has('message'))
-                                @component('front.components.error')
-                                    {{ $errors->first('message') }}
-                                @endcomponent
-                            @endif
-    						<textarea name="message" id="message" class="full-width" placeholder="@lang('Your message')" value="{{ old('message') }}" required></textarea>
-    					</div>
-    					<button type="submit" class="submit button-primary">@lang('Submit')</button>
-    				</form>
-                @else
-                    <em>@lang('You must be logged to add a comment !')</em>
-                @endif
-
-			</div> <!-- Respond End -->
+	    @include('front._post.respond')
 
 		</div> <!-- end row comments -->
 	</div> <!-- end comments-wrap -->
