6d7e0650ea71e9ea2d7e7f4403bd5dbbbec7cea8
imanghafoori <imanghafoori1@gmail.com>
2017-12-01 19:48:07 +0330

    extract partial

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -2,49 +2,7 @@
 <!--[if IE 8 ]><html class="no-js oldie ie8" lang="en"> <![endif]-->
 <!--[if IE 9 ]><html class="no-js oldie ie9" lang="en"> <![endif]-->
 <!--[if (gte IE 9)|!(IE)]><!--><html class="no-js" lang="{{ config('app.locale') }}"> <!--<![endif]-->
-<head>
-
-	<!--- basic page needs
-	================================================== -->
-	<meta charset="utf-8">
-	<title>{{ isset($post) && $post->seo_title ? $post->seo_title :  __(lcfirst('Title')) }}</title>
-	<meta name="description" content="{{ isset($post) && $post->meta_description ? $post->meta_description : __('description') }}">
-	<meta name="author" content="@lang(lcfirst ('Author'))">
-	@if(isset($post) && $post->meta_keywords)
-		<meta name="keywords" content="{{ $post->meta_keywords }}">
-	@endif
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-	<!-- mobile specific metas
-	================================================== -->
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-	<!-- CSS
-	================================================== -->
-	<link rel="stylesheet" href="{{ asset('css/base.css') }}">
-	<link rel="stylesheet" href="{{ asset('css/vendor.css') }}">
-	<link rel="stylesheet" href="{{ asset('css/main.css') }}">
-	@yield('css')
-
-	<style>
-		.search-wrap .search-form::after {
-			content: "@lang('Press Enter to begin your search.')";
-		}
-	</style>
-
-
-	<!-- script
-	================================================== -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min.js"></script>
-
-	<!-- favicons
-	================================================== -->
-	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-	<link rel="icon" href="favicon.ico" type="image/x-icon">
-
-</head>
-
+@include('front._layout.head')
 <body id="top">
 
 	<!-- header
