83e4dbd2e33b6e103ba930184ca7db2a5504a3f9
imanghafoori <imanghafoori1@gmail.com>
2017-12-01 19:51:42 +0330

    extract partial

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -19,22 +19,7 @@
 
 	   	@include('front._layout.main_nav')
 
-		<div class="search-wrap">
-			<form role="search" method="get" class="search-form" action="{{ route('posts.search') }}">
-				<label>
-					<input type="search" class="search-field" placeholder="@lang('Type Your Keywords')"  name="search" autocomplete="off" required>
-				</label>
-				<input type="submit" class="search-submit" value="">
-			</form>
-
-			<a href="#" id="close-search" class="close-btn">Close</a>
-
-		</div> <!-- end search wrap -->
-
-		<div class="triggers">
-			<a class="search-trigger" href="#"><i class="fa fa-search"></i></a>
-			<a class="menu-toggle" href="#"><span>Menu</span></a>
-		</div> <!-- end triggers -->
+        @include('front._layout.search_form')
 
    	</div>
 
